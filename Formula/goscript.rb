3fa16de8ca60321a2fc82f44688cf10865f6cfad
Aaron Parfitt <aaronparfitt123@gmail.com>
2025-09-20 12:15:45 +0100

    update

--- Formula/goscript.rb
+++ Formula/goscript.rb
@@ -1,8 +1,8 @@
 class Goscript < Formula
   desc "Dynamically Typed Language Written in Go"
   homepage "https://github.com/hyperioxx/GoScript"
-  url "https://github.com/hyperioxx/GoScript/archive/v0.1.0-alpha5.tar.gz"
-  sha256 "c58c73876d95a2843db0284e46f6d774ac63f878d9080c4394b8828ba9c2fc31"
+  url "https://github.com/hyperioxx/goscript/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "fb67be1639165423d80170938961cda56a6f29f348baa0bcd2de8533438c018b"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
